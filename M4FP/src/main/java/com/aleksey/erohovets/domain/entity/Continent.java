fdcfa4f7dcdd02dc35f8af1a926c59a10f59ebde
Aleksey <listat25@gmail.com>
2025-04-22 11:10:31 +0300

    Add files via upload

---
+++ M4FP/src/main/java/com/aleksey/erohovets/domain/entity/Continent.java
@@ -0,0 +1,11 @@
+package com.aleksey.erohovets.domain.entity;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
